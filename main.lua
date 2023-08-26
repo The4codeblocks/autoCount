bd8d5aea032ddde8cfcdcfd34c0062ee5e8d023f
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2023-08-26 09:17:47 +0200

    Make values look nicer

--- main.lua
+++ main.lua
@@ -2,13 +2,13 @@
 nitro = false
 
 --Variables
-charCount = -1
+charCount = 0
 n = 0
 nNew = n
 printed = ""
 
 --Main
-while (charCount + math.log10(math.max(nNew,1))) < ((nitro and 3999) or 1999) do
+while (charCount + math.log10(math.max(nNew,1))) < ((nitro and 4000) or 2000) do
     n = nNew
     printed = printed..n.."\n"
     charCount = charCount + math.floor(math.log10(math.max(n,1))) + 2
